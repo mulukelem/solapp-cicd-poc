5a4ce87d6ac68ea7beedb04cf14e057fd64158d2
Solomon Mulualem <smulualem@outlook.com>
2025-05-01 16:31:27 +0000

    app-req-updated

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from werkzeug.utils import quote  # Replaces deprecated url_quote
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask Web App!"
+    return "Hello from Modern Flask App!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
